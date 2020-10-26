58755eb8b56ab354d4f683aa6942b7535794074d
amatsukixprog <amatsukixprog@gmail.com>
2020-10-26 23:29:30 +0900

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by あまつき on 2020/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
